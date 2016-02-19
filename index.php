234c16a2c70da10b0ca9fc709243c4d07a40a2e7
Luiz <argentinaluiz@gmail.com>
2016-02-19 14:44:07 -0200

    Primeiro exemplo

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Minha página</title>
+</head>
+<body style="border: 1px solid">
+
+<h1>Bem-vindo a minha página</h1>
+
+<br/><br/><br/> <?php include 'header.php'; ?>
+<br/><br/><br/> <?php include 'content.php'; ?>
+<br/><br/><br/> <?php include 'footer.php'; ?>
+
+</body>
+</html>
